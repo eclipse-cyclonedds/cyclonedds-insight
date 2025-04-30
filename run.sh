72669733910930a0fb51b3d8c70bce7b71245463
Sven Trittler <sven@trittler.net>
2025-04-30 21:08:04 +0200

    add build script

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@ export CYCLONEDDS_PYTHON_HOME=$PWD/deps/cyclonedds-python
 
 export CYCLONEDDS_URI=file://$PWD/cyclone.xml
 
-
 source deps/venv/bin/activate && \
 pyside6-rcc ./resources.qrc -o ./src/qrc_file.py && \
 python3 ./src/main.py --loglevel=trace
