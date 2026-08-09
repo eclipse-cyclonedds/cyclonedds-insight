96a72d04fdeb892ad485c781a3d29af9c5f37c03
Sven Trittler <sven.trittler@icloud.com>
2026-08-09 15:07:18 +0200

    add language fr

--- run.bat
+++ run.bat
@@ -5,5 +5,6 @@ copy /Y .\deps\cyclonedds\etc\cyclonedds.xsd . && ^
 .\deps\venv\Scripts\pyside6-lrelease .\src\translations\cyclonedds-insight_en.ts && ^
 .\deps\venv\Scripts\pyside6-lrelease .\src\translations\cyclonedds-insight_de.ts && ^
 .\deps\venv\Scripts\pyside6-lrelease .\src\translations\cyclonedds-insight_nl.ts && ^
+.\deps\venv\Scripts\pyside6-lrelease .\src\translations\cyclonedds-insight_fr.ts && ^
 .\deps\venv\Scripts\pyside6-rcc .\resources.qrc -o .\src\qrc_file.py && ^
 python .\src\main.py --loglevel=trace
